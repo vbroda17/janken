633436cdef7c7abb7711be7bed5ec879a6f28526
vbroda17 <vbroda@vols.utk.edu>
2023-12-18 19:58:30 -0500

    playable

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,13 +12,23 @@ function getComputerChoice(){
 function playRound(player, computer=getComputerChoice()){
     p = player.toUpperCase();
     c = computer.toUpperCase();
-    const ROCK = "ROCK"
-    const PAPER = "PAPER"
-    const SCISSORS = "SCISSORS"
+    const ROCK = "ROCK";
+    const PAPER = "PAPER";
+    const SCISSORS = "SCISSORS";
     // console.log(p, c);
     // Win casses
-
+    if( (p === ROCK && c === SCISSORS) || (p === PAPER && c === ROCK) || (p === SCISSORS && c === PAPER) ) {
+        let resultString = `You WIN! ${p} beats ${c}.`;
+        return resultString;
+    }
     // loose casses
-
+    if( (c === ROCK && p === SCISSORS) || (c === PAPER && p === ROCK) || (c === SCISSORS && p === PAPER) ) {
+        let resultString = `You lose... ${p} beats ${c}.`;
+        return resultString;
+    }
     // Draws
+    if (p === c) {
+        let resultString = `Draw. ${p} splits with ${c}.`
+        return resultString;
+    }
 }
